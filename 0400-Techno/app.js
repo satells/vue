b4ae38cea338da59caf5fb4ae74aa606412a15bb
satells <msergiost@hotmail.com>
2022-09-22 17:27:09 -0300

    0400

---
+++ 0400-Techno/app.js
@@ -0,0 +1,39 @@
+const vm = new Vue({
+  el: "#app",
+
+  data: {
+    mensagem: "Vue está on-line",
+    produtos: [],
+    produto: {},
+  },
+  filters: {
+    numeroPreco(valor) {
+      return valor.toLocaleString("pt-BR", {
+        style: "currency",
+        currency: "BRL",
+      });
+    },
+  },
+  methods: {
+    fetchProdutos() {
+      fetch("./api/produtos.json")
+        .then((r) => r.json())
+        .then((r) => {
+          this.produtos = r;
+        });
+    },
+
+    fetchProduto(id) {
+      fetch(`./api/produtos/${id}/dados.json`)
+        .then((r) => r.json())
+        .then((r) => {
+          this.produto = r;
+        });
+    },
+  },
+  created() {
+    this.fetchProdutos();
+  },
+});
+
+console.log(vm);
